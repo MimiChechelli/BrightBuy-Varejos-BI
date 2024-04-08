c75088612a4be20fc37b18b993e466f0cdabd073
Michelli Patricia Luvison <126841298+MimiChechelli@users.noreply.github.com>
2024-04-08 14:41:51 -0300

    sub df

--- Análise.R
+++ Análise.R
@@ -1,5 +1,7 @@
-# Ler e montar planilha de trabalho
 library(readxl)
+library(dplyr)
+
+# Ler e montar planilha de trabalho
 vendas <- read_excel('fato_vendas.xlsx')
 familia <- read_excel('dim_familia_produtos.xlsx')
 vendedor <- read_excel('dim_vendedor.xlsx')
@@ -9,16 +11,95 @@ dados <- merge(vendas, vendedor, by="codigo_vendedor")
 dados <- merge(dados, produtos, by="codigo_produto")
 dados <- merge(dados, familia, by="codigo_familia")
 
+dados$lucro <- ((dados$preco_venda_unitario - dados$custo_produto_unitario) * dados$quantidade) - dados$valor_desconto
+
+# Total vendas ao longo do ano:
+total_monetario_anual <- sum(dados$valor_monetario_total)
+total_produtos_anual <- sum(dados$quantidade)
+total_lucro_anual <- sum(dados$lucro)
+
+# Total vendas por mês
+dados <- mutate(dados, Mes = format(data_venda, "%Y-%m"))
+
+monetario_por_mes <- dados %>%
+  group_by(Mes) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+produtos_por_mes <- dados %>%
+  group_by(Mes) %>%
+  summarise(Total= sum(quantidade))
+
+lucro_por_mes <- dados %>%
+  group_by(Mes) %>%
+  summarise(Total= sum(dados$lucro))
+
+# Total vendas por semana
+dados <- mutate(dados, Semana = format(data_venda, "%Y-%U"))
+
+monetario_por_semana <- dados %>%
+  group_by(Semana) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+produtos_por_semana <- dados %>%
+  group_by(Semana) %>%
+  summarise(Total= sum(quantidade))
+
+lucro_por_semana <- dados %>%
+  group_by(Semana) %>%
+  summarise(Total= sum(lucro))
+
+# Total vendas por filial adc por mes
+monetario_por_filial <- dados %>%
+  group_by(filial_venda) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+produtos_por_filial <- dados %>%
+  group_by(filial_venda) %>%
+  summarise(Total= sum(quantidade))
+
+lucro_por_filial <- dados %>%
+  group_by(filial_venda) %>%
+  summarise(Total= sum(lucro))
+
+# Total vendas por vendedor adc por mes
+monetario_por_vendedor <- dados %>%
+  group_by(codigo_vendedor) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+produtos_por_vendedor <- dados %>%
+  group_by(codigo_vendedor) %>%
+  summarise(Total= sum(quantidade))
+
+lucro_por_vendedor <- dados %>%
+  group_by(codigo_vendedor) %>%
+  summarise(Total= sum(lucro))
+
+# Total vendas por familia de produtos adc por mes
+monetario_por_fam_produto <- dados %>%
+  group_by(codigo_familia) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+produtos_por_fam_produto <- dados %>%
+  group_by(codigo_familia) %>%
+  summarise(Total= sum(quantidade))
+
+lucro_por_fam_produto <- dados %>%
+  group_by(codigo_familia) %>%
+  summarise(Total= sum(lucro))
+
+
+
+
+
+
+
+
+
+
 
 
 
 '''Análise de dados: 
-I. Relatório de vendas ao longo do ano, por mês e por semana.
-total R$, n e lucro vendas
-produtos por filial, vendedor
-total R$, n e lucro por vendedor, filial, produtos, familia de produtos
-no ano todo, por mes e por semana
-
 II. Indicação de cinco (5) famílias de produtos para focar nas campanhas de marketing em dezembro, 
 apartir do histórico e tendencias de fim de ano.
 
@@ -28,17 +109,4 @@ grafica geral e tratativa para melhorar as vendas dos vendedores com desempenho 
 
 IV. Calculo do ticket médio por filial, Churn Rate por vendedor/filial/geral, Lifetime Value (LTV) dos 
 Cliente individualmente e em média, Taxa de Recorrência e Margens de Lucro.'''
-
-
-
-
-
-
-
-
-
-
-
-
-
 
