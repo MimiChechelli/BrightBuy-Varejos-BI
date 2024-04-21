628c78e6763858b5a1cfef5cdecbfc0055c4cf81
Michelli Patricia Luvison <126841298+MimiChechelli@users.noreply.github.com>
2024-04-20 21:42:08 -0300

    Add files via upload

---
+++ BrightBuy-Varejos-BI/Análise.R
@@ -0,0 +1,215 @@
+library(readxl)
+library(dplyr)
+
+# Ler e montar planilha de trabalho
+vendas <- read_excel('fato_vendas.xlsx')
+familia <- read_excel('dim_familia_produtos.xlsx')
+vendedor <- read_excel('dim_vendedor.xlsx')
+produtos <- read_excel('dim_produtos.xlsx')
+
+dados <- merge(vendas, vendedor, by="codigo_vendedor")
+dados <- merge(dados, produtos, by="codigo_produto")
+dados <- merge(dados, familia, by="codigo_familia")
+
+dados$lucro <- ((dados$preco_venda_unitario - dados$custo_produto_unitario) * dados$quantidade) - dados$valor_desconto
+
+# Total vendas ao longo do ano:
+total_monetario_anual <- sum(dados$valor_monetario_total)
+total_produtos_anual <- sum(dados$quantidade)
+total_lucro_anual <- sum(dados$lucro)
+
+# Total vendas por mês
+dados <- mutate(dados, Mes = format(data_venda, "%Y-%m"))
+
+monetario_por_mes <- dados %>%
+  group_by(Mes) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+produtos_por_mes <- dados %>%
+  group_by(Mes) %>%
+  summarise(Total= sum(quantidade))
+
+lucro_por_mes <- dados %>%
+  group_by(Mes) %>%
+  summarise(Total= sum(dados$lucro))
+
+# Total vendas por semana
+dados <- mutate(dados, Semana = format(data_venda, "%Y-%U"))
+
+monetario_por_semana <- dados %>%
+  group_by(Semana) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+produtos_por_semana <- dados %>%
+  group_by(Semana) %>%
+  summarise(Total= sum(quantidade))
+
+lucro_por_semana <- dados %>%
+  group_by(Semana) %>%
+  summarise(Total= sum(lucro))
+
+# Total vendas por filial
+monetario_por_filial <- dados %>%
+  group_by(filial_venda) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+monetario_por_filial_mes <- dados %>%
+  group_by(filial_venda, Mes) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+monetario_por_filial_semana <- dados %>%
+  group_by(filial_venda, Semana) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+produtos_por_filial <- dados %>%
+  group_by(filial_venda) %>%
+  summarise(Total= sum(quantidade))
+
+produtos_por_filial_mes <- dados %>%
+  group_by(filial_venda, Mes) %>%
+  summarise(Total= sum(quantidade))
+
+produtos_por_filial_semana <- dados %>%
+  group_by(filial_venda, Semana) %>%
+  summarise(Total= sum(quantidade))
+
+lucro_por_filial <- dados %>%
+  group_by(filial_venda) %>%
+  summarise(Total= sum(lucro))
+
+lucro_por_filial_mes <- dados %>%
+  group_by(filial_venda, Mes) %>%
+  summarise(Total= sum(lucro))
+
+lucro_por_filial_semana <- dados %>%
+  group_by(filial_venda, Semana) %>%
+  summarise(Total= sum(lucro))
+
+# Total vendas por vendedor
+monetario_por_vendedor <- dados %>%
+  group_by(codigo_vendedor) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+produtos_por_vendedor <- dados %>%
+  group_by(codigo_vendedor) %>%
+  summarise(Total= sum(quantidade))
+
+lucro_por_vendedor <- dados %>%
+  group_by(codigo_vendedor) %>%
+  summarise(Total= sum(lucro))
+
+monetario_por_vendedor_mes <- dados %>%
+  group_by(codigo_vendedor, Mes) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+produtos_por_vendedor_mes <- dados %>%
+  group_by(codigo_vendedor, Mes) %>%
+  summarise(Total= sum(quantidade))
+
+lucro_por_vendedor_mes <- dados %>%
+  group_by(codigo_vendedor, Mes) %>%
+  summarise(Total= sum(lucro))
+
+monetario_por_vendedor_semana <- dados %>%
+  group_by(codigo_vendedor, Semana) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+produtos_por_vendedor_semana <- dados %>%
+  group_by(codigo_vendedor, Semana) %>%
+  summarise(Total= sum(quantidade))
+
+lucro_por_vendedor_semana <- dados %>%
+  group_by(codigo_vendedor, Semana) %>%
+  summarise(Total= sum(lucro))
+
+# Total vendas por familia de produtos
+monetario_por_fam_produto <- dados %>%
+  group_by(codigo_familia) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+produtos_por_fam_produto <- dados %>%
+  group_by(codigo_familia) %>%
+  summarise(Total= sum(quantidade))
+
+lucro_por_fam_produto <- dados %>%
+  group_by(codigo_familia) %>%
+  summarise(Total= sum(lucro))
+
+  monetario_por_fam_produto_mes <- dados %>%
+  group_by(codigo_familia, Mes) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+produtos_por_fam_produto_mes <- dados %>%
+  group_by(codigo_familia, Mes) %>%
+  summarise(Total= sum(quantidade))
+
+lucro_por_fam_produto_mes <- dados %>%
+  group_by(codigo_familia, Mes) %>%
+  summarise(Total= sum(lucro))
+
+monetario_por_fam_produto_semana <- dados %>%
+  group_by(codigo_familia, Semana) %>%
+  summarise(Total= sum(valor_monetario_total))
+
+produtos_por_fam_produto_semana <- dados %>%
+  group_by(codigo_familia, Semana) %>%
+  summarise(Total= sum(quantidade))
+
+lucro_por_fam_produto_semana <- dados %>%
+  group_by(codigo_familia, Semana) %>%
+  summarise(Total= sum(lucro))
+
+# Calculo do ticket médio geral anual 
+n_vendas_anuais <- nrow(dados)
+ticket_medio_anual_geral <- total_monetario_anual/n_vendas_anuais
+
+# Calculo do ticket médio geral mensal
+ticket_medio_mensal_geral <- data.frame(
+  Mes = unique(dados$Mes),
+  Ticket = numeric(length(unique(dados$Mes)))
+)
+
+for (i in seq_along(ticket_medio_mensal_geral$Mes)) {
+  mes <- ticket_medio_mensal_geral$Mes[i]
+  dados_mes_x <- dados[dados$Mes == mes, ]
+  valor_monetario <- monetario_por_mes$Total[monetario_por_mes$Mes == mes] 
+  ticket_medio_mensal_geral$Ticket[i] <- valor_monetario / nrow(dados_mes_x)
+}
+
+# Calculo do ticket médio geral semanal
+ticket_medio_semanal_geral <- data.frame(
+  Semana = unique(dados$Semana),
+  Ticket = numeric(length(unique(dados$Semana)))
+)
+
+for (i in seq_along(ticket_medio_semanal_geral$Semana)) {
+  semana <- ticket_medio_semanal_geral$Semana[i]
+  dados_semana_x <- dados[dados$Semana == semana, ]
+  valor_monetario <- monetario_por_semana$Total[monetario_por_semana$Semana == semana]
+  ticket_medio_semanal_geral$Ticket[i] <- valor_monetario / nrow(dados_semana_x)
+}
+
+# Calculo do ticket médio por filial anual/mensal/semanal
+
+# Calculo do ticket médio por vendedor anual/mensal/semanal
+
+# Churn Rate por vendedor/filial/geral
+
+# Lifetime Value (LTV) dos Cliente individualmente e em média
+
+# Taxa de Recorrência vendedor/filial/geral
+
+# Margens de Lucro.vendedor/filial/geral
+
+
+
+
+
+
+
+
+
+
+'''Avaliação de desempenho de vendedores apartir da analise do número de produtos vendidos, 
+número de clientes, ticket médio e receita e lucro gerada. 5 melhores vendedores do ano, comparação 
+grafica geral e tratativa para melhorar as vendas dos vendedores com desempenho abaixo da média.'''
---
+++ BrightBuy-Varejos-BI/readme.md
@@ -0,0 +1,27 @@
+Este repositório contém os recursos e artefatos relacionados ao projeto de Business Intelligence (BI) da BrightBuy. 
+O objetivo deste projeto é fornecer insights acionáveis para apoiar a tomada de decisões estratégicas e operacionais por meio da análise de dados.
+
+Visão Geral
+O projeto de Business Intelligence visa processar e analisar dados provenientes das ultimas vendas.
+Utilizei ferramentas e técnicas avançadas para transformar esses dados em informações valiosas e compreensíveis.
+
+**********************************************************************************************************************
+Análise de dados: 
+I. Relatório de vendas ao longo do ano, por mês e por semana.
+total R$, n e lucro vendas
+produtos por filial, vendedor
+total R$, n e lucro por vendedor, filial, produtos, familia de produtos
+no ano todo, por mes e por semana
+
+II. Indicação de cinco (5) famílias de produtos para focar nas campanhas de marketing em dezembro, apartir do histórico e tendencias de fim de ano.
+
+III. Avaliação de desempenho de vendedores apartir da analise do número de produtos vendidos, número de clientes, ticket médio e receita e lucro gerada. 5 melhores vendedores do ano, com paração grafica geral e tratativa para melhorar as vendas dos vendedores com desempenho abaixo da média.
+
+IV. Calculo do ticket médio por filial, Churn Rate por vendedor/filial/geral, Lifetime Value (LTV) dos Cliente individualmente e em média, Taxa de Recorrência e Margens de Lucro.
+**********************************************************************************************************************
+
+E-mail: michiluvison@gmail.com
+LinkedIn: https://www.linkedin.com/in/michelli-luvison-623649268/
+Portfolio: https://sites.google.com/view/michelli-luvison/p%C3%A1gina-inicial
+
+Última atualização: Março de 2024
